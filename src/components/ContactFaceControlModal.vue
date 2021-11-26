8fe1f1193ad232dfdb27df2158ebc5fcb90f4086
Никита Горбухов <nekitenzo@gmail.com>
2021-11-26 12:42:23 +0300

    Перегнал на нормальную логику модалку клтиента

--- src/components/ContactFaceControlModal.vue
+++ src/components/ContactFaceControlModal.vue
@@ -1,122 +1,123 @@
 <template>
-  <v-dialog v-model="dialogContactFaces" max-width="500px">
-    <template v-slot:activator="{ on }">
-      <v-icon color="light-blue accent-3" dark v-on="on">person_add_alt</v-icon>
-    </template>
-    <v-card>
-      <v-card-title>
-        <span class="headline">{{ formTitleContactFaces }}</span>
-      </v-card-title>
+	<v-dialog v-model="dialogContactFaces" max-width="500px">
+		<template v-slot:activator="{ on }">
+			<v-icon v-on="on">
+				person_add_alt
+			</v-icon>
+		</template>
+		<v-card>
+			<v-card-title>
+				<span class="headline">{{ formTitleContactFaces }}</span>
+			</v-card-title>
 
-      <v-card-text>
-        <v-container grid-list-md>
-          <v-layout wrap>
-            <v-flex xs12 sm12 md12>
-              <v-text-field
-                v-model="contactFace.name"
-                label="ФИО"
-                hint="фамилия имя отчество через пробел"
-                color="light-blue accent-3"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm4 md4>
-              <v-text-field
-                v-model="contactFace.phone"
-                label="Телефон"
-                color="light-blue accent-3"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm8 md8>
-              <v-text-field
-                v-model="contactFace.email"
-                label="Email"
-                color="light-blue accent-3"
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
+			<v-card-text>
+				<v-container grid-list-md>
+					<v-layout wrap>
+						<v-flex xs12 sm12 md12>
+							<v-text-field
+								v-model="contactFace.name"
+								label="ФИО"
+								hint="фамилия имя отчество через пробел"
+								color="light-blue accent-3"
+							></v-text-field>
+						</v-flex>
+						<v-flex xs12 sm4 md4>
+							<v-text-field
+								v-model="contactFace.phone"
+								label="Телефон"
+								color="light-blue accent-3"
+							></v-text-field>
+						</v-flex>
+						<v-flex xs12 sm8 md8>
+							<v-text-field
+								v-model="contactFace.email"
+								label="Email"
+								color="light-blue accent-3"
+							></v-text-field>
+						</v-flex>
+					</v-layout>
+				</v-container>
+			</v-card-text>
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="light-blue accent-3" @click="closeContactFace"
-          >Отменить контакт</v-btn
-        >
-        <v-btn color="green accent-2" @click="saveContactFace"
-          >Сохранить контакт</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn color="light-blue accent-3" @click="closeContactFace"
+					>Отменить контакт</v-btn
+				>
+				<v-btn color="green accent-2" @click="saveContactFace"
+					>Сохранить контакт</v-btn
+				>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
 </template>
 
 <script>
 export default {
-  name: "ContactFaceControlModal",
-  data: () => ({
-    dialogContactFaces: false,
-    expanded: [],
-    clients: [],
-    contactFaces: [],
-    editedContactFaceIndex: -1,
+	name: 'ContactFaceControlModal',
+	props: ['currentContactFace'],
+	data: () => ({
+		dialogContactFaces: false,
+		expanded: [],
 
-    contactFace: {
-      name: "",
-      email: "",
-      phone: "",
-    },
+		editedContactFaceIndex: -1,
+		contactFace: {
+			name: '',
+			email: '',
+			phone: '',
+		},
 
-    defaultContactFace: {
-      name: "",
-      email: "",
-      phone: "",
-    },
-  }),
+		defaultContactFace: {
+			name: '',
+			email: '',
+			phone: '',
+		},
+	}),
 
-  computed: {
-    formTitleContactFaces() {
-      return this.editedContactFaceIndex === -1
-        ? "Новый контакт"
-        : "Редактирование контакта";
-    },
-  },
+	computed: {
+		formTitleContactFaces() {
+			return this.editedContactFaceIndex === -1
+				? 'Новый контакт'
+				: 'Редактирование контакта';
+		},
+	},
 
-  watch: {
-    dialogContactFaces(val) {
-      console.log("DialogContactFaces.val =", val);
-      val || this.closeContactFace();
-    },
-  },
+	watch: {
+		dialogContactFaces(val) {
+			console.log('DialogContactFaces.val =', val);
+			val || this.closeContactFace();
+		},
+	},
 
-  methods: {
-    saveContactFace() {
-      if (this.editedContactFaceIndex > -1) {
-        Object.assign(
-          this.clients[this.editedContactFaceIndex],
-          this.contactFace
-        );
-      } else {
-        this.contactFaces.push(this.contactFace);
-      }
-      this.close();
-      this.dialogContactFaces = false;
-    },
+	methods: {
+		saveContactFace() {
+			if (this.editedContactFaceIndex > -1) {
+				Object.assign(
+					this.clients[this.editedContactFaceIndex],
+					this.contactFace,
+				);
+			} else {
+				this.contactFaces.push(this.contactFace);
+			}
+			this.close();
+			this.dialogContactFaces = false;
+		},
 
-    closeContactFace() {
-      this.dialogContactFaces = false;
-      setTimeout(() => {
-        this.contactFace = Object.assign({}, this.defaultContactFace);
-        this.editedContactFaceIndex = -1;
-      }, 300);
-    },
+		closeContactFace() {
+			this.dialogContactFaces = false;
+			setTimeout(() => {
+				this.contactFace = Object.assign({}, this.defaultContactFace);
+				this.editedContactFaceIndex = -1;
+			}, 300);
+		},
 
-    // editContactFaces(item) {
-    //   console.log("editedContactFaces", item);
-    //   this.editedContactFaceIndex = this.clients.indexOf(item);
-    //   this.contactFace = Object.assign({}, item);
-    //   this.dialogContactFaces = true;
+		// editContactFaces(item) {
+		//   console.log("editedContactFaces", item);
+		//   this.editedContactFaceIndex = this.clients.indexOf(item);
+		//   this.contactFace = Object.assign({}, item);
+		//   this.dialogContactFaces = true;
 
-    // },
-  },
+		// },
+	},
 };
 </script>
--- src/pages/ClientsPage.vue
+++ src/pages/ClientsPage.vue
@@ -1,108 +1,155 @@
 <template>
-  <div>
-    <v-toolbar color="white">
-      <v-toolbar-title>Все клиенты</v-toolbar-title>
-      <v-divider class="mx-3" inset vertical></v-divider>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        color="light-blue accent-3"
-        hide-details
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <ClientControlModal />
-    </v-toolbar>
+	<div>
+		<v-toolbar color="white">
+			<v-toolbar-title>Все клиенты</v-toolbar-title>
+			<v-divider class="mx-3" inset vertical></v-divider>
+			<v-spacer></v-spacer>
+			<v-text-field
+				v-model="search"
+				append-icon="mdi-magnify"
+				label="Search"
+				single-line
+				color="light-blue accent-3"
+				hide-details
+			></v-text-field>
+			<v-spacer></v-spacer>
+		</v-toolbar>
 
-    <v-data-table
-      :headers="headers"
-      :items="clients"
-      :search="search"
-      item-key="name"
-      class="elevation-1"
-      :expanded.sync="expanded"
-      show-expand
-    >
-      <template v-slot:item.status="{ item }" dark>
-        <v-chip :color="getColor(item.status)">
-          {{ item.status }}
-        </v-chip>
-      </template>
+		<v-data-table
+			:headers="headers"
+			:items="clientsList"
+			:search="search"
+			item-key="id"
+			class="elevation-1"
+			:expanded.sync="expanded"
+			show-expand
+		>
+			<template v-slot:item.status="{ item }" dark>
+				<v-chip :color="getColor(item.status)">
+					{{ item.status }}
+				</v-chip>
+			</template>
 
-      <template v-slot:item.addContactFace="{}">
-        <ContactFaceControlModal />
-      </template>
+			<template v-slot:item.addContactFace="{}">
+				<ContactFaceControlModal
+					v-if="contactFacesModalVisible"
+					:currentContactFace.sync="currentContactFace"
+				/>
+			</template>
 
-      <template v-slot:item.edit="{ item }">
-        <v-icon color="green darken-1" @click="editClient(item)"> edit </v-icon>
-      </template>
-      <template v-slot:item.delete="{ item }">
-        <v-icon color="red" @click="deleteClient(item)"> delete </v-icon>
-      </template>
+			<template v-slot:item.edit="{ item }">
+				<v-icon color="green darken-1" @click="startEditClient(item)">
+					edit
+				</v-icon>
+			</template>
+			<template v-slot:item.delete="{ item }">
+				<v-icon color="red" @click="deleteClient(item)">
+					delete
+				</v-icon>
+			</template>
 
-      <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">
-          Контактные лица {{ item.name }}: {{ contactFace.name }}
-          {{ contactFace.phone }} {{ contactFace.email }}
-        </td>
-      </template>
-    </v-data-table>
-  </div>
+			<template v-slot:expanded-item="{ headers, item }">
+				<td :colspan="headers.length">
+					{{ item.contactFaces }}
+					<!-- Контактные лица {{ item.name }}: {{ contactFace.name }}
+					{{ contactFace.phone }} {{ contactFace.email }} -->
+				</td>
+			</template>
+		</v-data-table>
+		<v-btn
+			@click="startCreateClient"
+			color="light-blue accent-3"
+			dark
+			class="mt-2"
+		>
+			+ Новый клиент
+		</v-btn>
+		<ClientControlModal
+			v-if="clientModalVisible"
+			ref="clientControlModal"
+			:currentClient.sync="currentClient"
+			@done="onCliendModalDone"
+		/>
+	</div>
 </template>
 
 <script>
-import ClientControlModal from "@/components/ClientControlModal";
-import ContactFaceControlModal from "@/components/ContactFaceControlModal";
+import ClientControlModal from '@/components/ClientControlModal';
+import ContactFaceControlModal from '@/components/ContactFaceControlModal';
 export default {
-  name: "ClientsPage",
-  components: {
-    ClientControlModal,
-    ContactFaceControlModal,
-  },
-  data: () => ({
-    search: "",
-    isCompany: false,
-    dialog: false,
-    dialogContactFaces: false,
-    expanded: [],
-    headers: [
-      { text: "Cтатус", value: "status" },
-      { text: "ФИО", align: "left", sortable: false, value: "name" },
-      { text: "Email", value: "email" },
-      { text: "Телефон", value: "phone" },
-      { text: "Город", value: "city" },
-      { text: "ИНН", value: "inn" },
-      { text: "fax", value: "fax" },
-      { text: "Адрес", value: "address" },
-      { text: "Компания", value: "companyId" },
-      { text: "ОГРН", value: "ogrn" },
+	name: 'ClientsPage',
+	components: {
+		ClientControlModal,
+		ContactFaceControlModal,
+	},
+	data: () => ({
+		search: '',
+		isCompany: false,
+		clientModalMode: 'create',
+		contactFacesModalMode: 'create',
+		clientModalVisible: false,
+		contactFacesModalVisible: false,
+		expanded: [],
+		headers: [
+			{ text: 'Cтатус', value: 'status' },
+			{ text: 'ФИО', align: 'left', sortable: false, value: 'name' },
+			{ text: 'Email', value: 'email' },
+			{ text: 'Телефон', value: 'phone' },
+			{ text: 'Город', value: 'city' },
+			{ text: 'ИНН', value: 'inn' },
+			{ text: 'fax', value: 'fax' },
+			{ text: 'Адрес', value: 'address' },
+			{ text: 'Компания', value: 'companyId' },
+			{ text: 'ОГРН', value: 'ogrn' },
 
-      { text: "", value: "addContactFace", sortable: false },
-      { text: "", value: "edit", sortable: false },
-      { text: "", value: "delete", sortable: false },
-    ],
-  }),
+			{ text: '', value: 'addContactFace', sortable: false },
+			{ text: '', value: 'edit', sortable: false },
+			{ text: '', value: 'delete', sortable: false },
+		],
+		clientsList: [],
+		currentClient: null,
+		currentContactFace: null,
+	}),
 
-  methods: {
-    getColor(status) {
-      if (status == "CURRENT") return "green accent-2";
-      else if (status == "INACTIVE") return "grey lighten-1";
-      else if (status == "FUTURE") return "cyan accent-1";
-    },
+	methods: {
+		startCreateClient() {
+			this.clientModalVisible = true;
+			this.$nextTick(() => {
+				this.$refs.clientControlModal.openModal();
+			});
+		},
+		startEditClient(item) {
+			this.clientModalVisible = true;
+			this.currentClient = item;
+			this.$nextTick(() => {
+				this.$refs.clientControlModal.openModal(item);
+			});
+		},
+		onCliendModalDone(client) {
+			if (client) {
+				const index = this.clientsList.findIndex(
+					({ id }) => client.id === id,
+				);
+				if (index > -1) {
+					this.clientsList[index] = client;
+				} else {
+					this.clientsList = this.clientsList.concat(client);
+				}
+			}
+			this.currentClient = null;
+			this.clientModalVisible = false;
+		},
+		getColor(status) {
+			if (status == 'CURRENT') return 'green accent-2';
+			else if (status == 'INACTIVE') return 'grey lighten-1';
+			else if (status == 'FUTURE') return 'cyan accent-1';
+		},
 
-    editClient(item) {
-      this.editedClientIndex = this.clients.indexOf(item);
-      this.editedClient = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteClient(item) {
-      const index = this.clients.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.clients.splice(index, 1);
-    },
-  },
+		deleteClient(item) {
+			const index = this.clientsList.indexOf(item);
+			confirm('Are you sure you want to delete this item?') &&
+				this.clientsList.splice(index, 1);
+		},
+	},
 };
 </script>
