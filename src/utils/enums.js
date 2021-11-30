5d23482d472de7df734a46cff35311b552373254
Anton <ermili4ev.tony2@yandex.ru>
2021-12-01 01:11:59 +0300

    enums done

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -3,5 +3,34 @@ const CLIENT_STATUSES = {
     'FUTURE': 'Будущий',
     'INACTIVE': 'Неактивен'
 }
+  
+  const CONTACT_FACE_STATUS = {
+    ACTIVE = 'Текущий',
+    INACTIVE = 'Неактивный'
+  }
+  
+  const PRIORITY = {
+    LOW = 'Низкий',
+    MEDIUM = 'Средний',
+    HIGH = 'Высокий'
+  }
+  
+  const TASK_ROLE = {
+    CREATOR = 'Создатель',
+    EXECUTOR = 'Исполнитель'
+  }
+  
+  const TASK_STATUS = {
+    ACTIVE = 'Текущее',
+    READY = 'Готовое',
+    INACTIVE = 'Неактивное'
+  }
+  
+  const EMPLOYEE_ROLE = {
+    MANAGER = 'Менеджер',
+    COMMON = 'Рядовой',
+    ADMIN = 'Админ',
+    INACTIVE = 'Неактивный'
+  }
 
-export { CLIENT_STATUSES }
+export { CLIENT_STATUSES, CONTACT_FACE_STATUS, PRIORITY, TASK_ROLE, TASK_STATUS, EMPLOYEE_ROLE }
