1c300193106ee21b705046214de9561e13234d02
alisemor21 <morozova.alisa.2001@yandex.ru>
2021-11-30 17:14:06 +0300

    reportsService

--- src/netClient/reportService.js
+++ src/netClient/reportService.js
@@ -1,1 +1,76 @@
-import http from '@/netClient/config';
+import http from "@/netClient/config";
+
+export async function getAllReports() {
+  try {
+    const response = await http.get("/api/reports", {
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.accessToken,
+      },
+    });
+    return response.data.tasks ?? [];
+  } catch (error) {
+    console.error({ error });
+    throw error;
+  }
+}
+
+export async function getReportById(id) {
+  try {
+    const response = await http.get("/api/reports", {
+      params: {
+        id,
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.accessToken,
+      },
+    });
+    return response.data ?? [];
+  } catch (error) {
+    console.error({ error });
+    throw error;
+  }
+}
+
+export async function createReport(employee, startDate, endDate) {
+  try {
+    const response = await http.post(
+      "/api/reports",
+      {
+        employee,
+        startDate,
+        endDate,
+      },
+      {
+        headers: {
+          "Contentt-Type": "application/json",
+          "x-access-token": localStorage.accessToken,
+        },
+      }
+    );
+    return response.data ?? [];
+  } catch (error) {
+    console.error({ error });
+    throw error;
+  }
+}
+
+
+export async function downloadReport(id) {
+    try {
+      const response = await http.get("/api/reports/download", {
+        params: {
+          id,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": localStorage.accessToken,
+        },
+      });
+      return response.data ?? [];
+    } catch (error) {
+      console.error({ error });
+      throw error;
+    }
+  }
