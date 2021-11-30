093a37e5e1c924969fd08f93f4a2db8b3c420629
Anton <ermili4ev.tony2@yandex.ru>
2021-12-01 01:14:45 +0300

    govno in enums fixed

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -5,32 +5,32 @@ const CLIENT_STATUSES = {
 }
   
   const CONTACT_FACE_STATUS = {
-    ACTIVE = 'Текущий',
-    INACTIVE = 'Неактивный'
+    'ACTIVE': 'Текущий',
+    'INACTIVE': 'Неактивный'
   }
   
   const PRIORITY = {
-    LOW = 'Низкий',
-    MEDIUM = 'Средний',
-    HIGH = 'Высокий'
+    'LOW': 'Низкий',
+    'MEDIUM': 'Средний',
+    'HIGH': 'Высокий'
   }
   
   const TASK_ROLE = {
-    CREATOR = 'Создатель',
-    EXECUTOR = 'Исполнитель'
+    'CREATOR': 'Создатель',
+    'EXECUTOR': 'Исполнитель'
   }
   
   const TASK_STATUS = {
-    ACTIVE = 'Текущее',
-    READY = 'Готовое',
-    INACTIVE = 'Неактивное'
+    'ACTIVE': 'В процессе',
+    'READY': 'Выполнено',
+    'INACTIVE': 'Неактивное'
   }
   
   const EMPLOYEE_ROLE = {
-    MANAGER = 'Менеджер',
-    COMMON = 'Рядовой',
-    ADMIN = 'Админ',
-    INACTIVE = 'Неактивный'
+    'MANAGER': 'Менеджер',
+    'COMMON': 'Рядовой',
+    'ADMIN': 'Админ',
+    'INACTIVE': 'Неактивный'
   }
 
 export { CLIENT_STATUSES, CONTACT_FACE_STATUS, PRIORITY, TASK_ROLE, TASK_STATUS, EMPLOYEE_ROLE }
